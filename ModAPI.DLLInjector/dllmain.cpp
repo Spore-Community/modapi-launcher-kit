66b1d52422c2c2852df310bc2a9e52a88f052880
Zachary Lockwood <bestgoaltenderever@gmail.com>
2025-02-11 22:45:57 -0800

    small cleanup, update ModApiUpdate.zip

--- ModAPI.DLLInjector/dllmain.cpp
+++ ModAPI.DLLInjector/dllmain.cpp
@@ -45,7 +45,6 @@ static void (WINAPI* GetStartupInfoA_real)(LPSTARTUPINFOA) = GetStartupInfoA;
 static void GetStartupInfoA_detoured(LPSTARTUPINFOA lpStartupInfo)
 {
     static bool injected = false;
-
     if (!injected)
     {
         injected = true;
@@ -90,17 +89,14 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
-        //MessageBoxA(NULL, "attach debugger", "spore", MB_OK | MB_ICONERROR);
         DetourRestoreAfterWith();
         DetourTransactionBegin();
-        DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)GetStartupInfoA_real, (PVOID)GetStartupInfoA_detoured);
         DetourTransactionCommit();
     }
     else if (ul_reason_for_call == DLL_PROCESS_DETACH)
     {
         DetourTransactionBegin();
-        DetourUpdateThread(GetCurrentThread());
         DetourDetach(&(PVOID&)GetStartupInfoA_real, (PVOID)GetStartupInfoA_detoured);
         if (InitializeGeneralAllocator_real) DetourDetach(&(PVOID&)InitializeGeneralAllocator_real, (PVOID)InitializeGeneralAllocator_detoured);
         if (DisposeGeneralAllocator_real) DetourDetach(&(PVOID&)DisposeGeneralAllocator_real, (PVOID)DisposeGeneralAllocator_detoured);
