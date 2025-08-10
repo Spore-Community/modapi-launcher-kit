47e26bb69c78df0813a25b7bada95d2ee01def1b
0KepOnline <78259495+0KepOnline@users.noreply.github.com>
2025-08-10 11:01:01 +0300

    Update `GameVersionSelector` form, replace "Disk" with "Disc" in most places

--- Spore ModAPI Launcher/GameVersionSelector.Designer.cs
+++ Spore ModAPI Launcher/GameVersionSelector.Designer.cs
@@ -29,68 +29,84 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameVersionSelector));
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelDetectionError = new System.Windows.Forms.Label();
             this.btnSteam = new System.Windows.Forms.Button();
-            this.btnOrigin = new System.Windows.Forms.Button();
-            this.btnDisk = new System.Windows.Forms.Button();
+            this.btnEAApp = new System.Windows.Forms.Button();
+            this.btnDisc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // labelDetectionError
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(503, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = Strings.ChooseGameVersion;
+            this.labelDetectionError.AutoSize = true;
+            this.labelDetectionError.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDetectionError.Location = new System.Drawing.Point(12, 9);
+            this.labelDetectionError.Name = "labelDetectionError";
+            this.labelDetectionError.Size = new System.Drawing.Size(399, 60);
+            this.labelDetectionError.TabIndex = 0;
+            this.labelDetectionError.Text = "Your game version could not be detected automatically.\r\n\r\nPlease choose your vers" +
+    "ion:";
             // 
             // btnSteam
             // 
-            this.btnSteam.BackgroundImage = global::SporeModAPI_Launcher.Properties.Resources.VersionButton_Steam_GoG1;
             this.btnSteam.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnSteam.Location = new System.Drawing.Point(13, 133);
+            this.btnSteam.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSteam.Image = global::SporeModAPI_Launcher.Properties.Resources.VersionButton_Steam_GOG_Icon;
+            this.btnSteam.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSteam.Location = new System.Drawing.Point(12, 167);
             this.btnSteam.Name = "btnSteam";
-            this.btnSteam.Size = new System.Drawing.Size(538, 80);
+            this.btnSteam.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.btnSteam.Size = new System.Drawing.Size(539, 80);
             this.btnSteam.TabIndex = 3;
+            this.btnSteam.Text = "Steam / GOG";
+            this.btnSteam.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnSteam.UseVisualStyleBackColor = true;
             this.btnSteam.Click += new System.EventHandler(this.btnSteam_Click);
             // 
-            // btnOrigin
+            // btnEAApp
             // 
-            this.btnOrigin.BackgroundImage = global::SporeModAPI_Launcher.Properties.Resources.VersionButton_Origin1;
-            this.btnOrigin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnOrigin.Location = new System.Drawing.Point(13, 219);
-            this.btnOrigin.Name = "btnOrigin";
-            this.btnOrigin.Size = new System.Drawing.Size(538, 80);
-            this.btnOrigin.TabIndex = 2;
-            this.btnOrigin.UseVisualStyleBackColor = true;
-            this.btnOrigin.Click += new System.EventHandler(this.btnOrigin_Click);
+            this.btnEAApp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnEAApp.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEAApp.Image = global::SporeModAPI_Launcher.Properties.Resources.VersionButton_EAApp_Icon;
+            this.btnEAApp.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEAApp.Location = new System.Drawing.Point(12, 253);
+            this.btnEAApp.Name = "btnEAApp";
+            this.btnEAApp.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.btnEAApp.Size = new System.Drawing.Size(539, 80);
+            this.btnEAApp.TabIndex = 2;
+            this.btnEAApp.Text = "EA App";
+            this.btnEAApp.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEAApp.UseVisualStyleBackColor = true;
+            this.btnEAApp.Click += new System.EventHandler(this.btnEAApp_Click);
             // 
-            // btnDisk
+            // btnDisc
             // 
-            this.btnDisk.BackgroundImage = global::SporeModAPI_Launcher.Properties.Resources.VersionButton_Disk3;
-            this.btnDisk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnDisk.Location = new System.Drawing.Point(13, 47);
-            this.btnDisk.Name = "btnDisk";
-            this.btnDisk.Size = new System.Drawing.Size(538, 80);
-            this.btnDisk.TabIndex = 1;
-            this.btnDisk.UseVisualStyleBackColor = true;
-            this.btnDisk.Click += new System.EventHandler(this.btnDisk_Click);
+            this.btnDisc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDisc.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDisc.Image = global::SporeModAPI_Launcher.Properties.Resources.VersionButton_Disc_Icon;
+            this.btnDisc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDisc.Location = new System.Drawing.Point(12, 81);
+            this.btnDisc.Name = "btnDisc";
+            this.btnDisc.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.btnDisc.Size = new System.Drawing.Size(539, 80);
+            this.btnDisc.TabIndex = 1;
+            this.btnDisc.Text = "Disc";
+            this.btnDisc.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDisc.UseVisualStyleBackColor = true;
+            this.btnDisc.Click += new System.EventHandler(this.btnDisc_Click);
             // 
             // GameVersionSelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 313);
+            this.ClientSize = new System.Drawing.Size(563, 345);
             this.Controls.Add(this.btnSteam);
-            this.Controls.Add(this.btnOrigin);
-            this.Controls.Add(this.btnDisk);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEAApp);
+            this.Controls.Add(this.btnDisc);
+            this.Controls.Add(this.labelDetectionError);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GameVersionSelector";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = Strings.ChooseGameVersionTitle;
+            this.Text = "Select your game version";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,9 +114,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnDisk;
-        private System.Windows.Forms.Button btnOrigin;
+        private System.Windows.Forms.Label labelDetectionError;
+        private System.Windows.Forms.Button btnDisc;
+        private System.Windows.Forms.Button btnEAApp;
         private System.Windows.Forms.Button btnSteam;
     }
 }
