54d483bc2dc042e82b39ec6a75f61d0596a5e28d
Rosalie Wanders <rosalie@mailbox.org>
2025-08-27 14:24:00 +0200

    ModAPI.Common: fix AssemblyInfo.cs

--- ModAPI.Common/Properties/AssemblyInfo.cs
+++ ModAPI.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModApi.Common")]
+[assembly: AssemblyTitle("ModAPI.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModApi.Common")]
+[assembly: AssemblyProduct("ModAPI.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
