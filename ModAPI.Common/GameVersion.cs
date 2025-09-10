db2403cafd552fd40dd5897a653dc7c7b9df031a
Rosalie Wanders <rosalie@mailbox.org>
2025-09-10 20:58:32 +0200

    ModAPI.Common: use more verbose names in GameVersion.cs

--- ModAPI.Common/GameVersion.cs
+++ ModAPI.Common/GameVersion.cs
@@ -7,15 +7,15 @@ namespace ModAPI.Common
         /// <summary>3.0.0.2818 (July 2009) installed from disc, with patch 5.1</summary>
         Disc,
         /// <summary>3.1.0.22 (March 2017) installed from Origin, requires ModAPI Fix</summary>
-        Origin_Patched,
+        Origin_March2017,
         /// <summary>3.1.0.29 (October 2024) installed from EA App or Origin, requires ModAPI Fix</summary>
-        EA_Oct24,
+        EA_October2024,
         /// <summary>3.1.0.22 (March 2017) installed from GOG or Steam</summary>
-        Steam_Patched,
+        Steam_March2017,
         /// <summary>3.1.0.29 (October 2024) installed from GOG</summary>
-        GoG_Oct24,
+        GOG_October2024,
         /// <summary>3.1.0.29 (October 2024) installed from Steam, has steamstub DRM</summary>
-        Steam_Oct24,
+        Steam_October2024,
 
         None
     }
@@ -25,38 +25,39 @@ namespace ModAPI.Common
 
 
         private static readonly int[] ExecutableSizes = { 
-                                       /* DISC*/    24909584,
-                                       /* ORIGIN_P */ 24898224,
-                                       /* EA_OCT24 */ 24906040,
-                                       /* STEAM_P */24885248,
-                                       /* GOG_OCT24 */24895536,
-                                       /* STEAM_OCT24 */ 25066744};
+                                                    24909584, // Disc
+                                                    24898224, // Origin_March2017
+                                                    24906040, // EA_October2024
+                                                    24885248, // Steam_March2017
+                                                    24895536, // GoG_October2024
+                                                    25066744  // Steam_October2024
+                                                };
 
         private static readonly string[] VersionNames = { 
-                                                  "disk", 
-                                                  "steam_patched",  // origin uses the steam_patched one
-                                                  "steam_patched",  // EA app uses the steam_patched one
-                                                  "steam_patched", 
-                                                  "steam_patched",  // in GoG executable, addresses did not change in October 2024 update
-                                                  "steam_patched",  // addresses did not change in October 2024 update, but the executable was protected with SteamDRM
-                                                  null
-                                              };
+                                                    "disk", 
+                                                    "steam_patched",  // Origin_March2017 uses the steam_patched one
+                                                    "steam_patched",  // EA_October2024 uses the steam_patched one
+                                                    "steam_patched", 
+                                                    "steam_patched",  // in GoG executable, addresses did not change in October 2024 update
+                                                    "steam_patched",  // addresses did not change in October 2024 update, but the executable was protected with SteamDRM
+                                                    null
+                                                };
 
         // Origin users download an alternative executable, so it uses a different name
         private static readonly string[] ExecutableNames = { 
-                                                  "SporeApp.exe", 
-                                                  "SporeApp_ModAPIFix.exe",  // origin uses a different one
-                                                  "SporeApp_ModAPIFix.exe",  // EA app uses a different one
-                                                  "SporeApp.exe", 
-                                                  "SporeApp.exe", 
-                                                  "SporeApp.exe",
-                                                  null
-                                              };
+                                                    "SporeApp.exe", 
+                                                    "SporeApp_ModAPIFix.exe",  // Origin_March2017 uses a different one
+                                                    "SporeApp_ModAPIFix.exe",  // EA_October2024 uses a different one
+                                                    "SporeApp.exe", 
+                                                    "SporeApp.exe", 
+                                                    "SporeApp.exe",
+                                                    null
+                                                };
 
         public static bool RequiresModAPIFix(GameVersionType versionType)
         {
-            return versionType == GameVersionType.Origin_Patched ||
-                    versionType == GameVersionType.EA_Oct24;
+            return versionType == GameVersionType.Origin_March2017 ||
+                    versionType == GameVersionType.EA_October2024;
         }
 
         public static GameVersionType DetectVersion(string path)
@@ -93,11 +94,11 @@ namespace ModAPI.Common
             {
                 return "SporeModAPI.disk.dll";
             }
-            else if ((type == GameVersionType.Origin_Patched) ||
-                     (type == GameVersionType.EA_Oct24) ||
-                     (type == GameVersionType.Steam_Patched) ||
-                     (type == GameVersionType.GoG_Oct24) ||
-                     (type == GameVersionType.Steam_Oct24))
+            else if ((type == GameVersionType.Origin_March2017) ||
+                     (type == GameVersionType.EA_October2024)  ||
+                     (type == GameVersionType.Steam_March2017) ||
+                     (type == GameVersionType.GOG_October2024) ||
+                     (type == GameVersionType.Steam_October2024))
             {
                 return "SporeModAPI.march2017.dll";
             }
